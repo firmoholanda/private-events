ee36ef2f96c944ef108193e67d5b06d2b28d3df0
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-27 11:11:09 -0500

    Setup from 1 to 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/sshow'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
