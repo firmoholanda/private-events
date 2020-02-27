71e2adb222c8cdd78825b8e9422c361ceaa4b550
firmo holanda <firmo@viize.com>
2020-02-27 19:26:46 -0300

    create items 1 to 10 on attendance feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :destroy]
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
