4b7a48b56542a3cdcffbf8c9eeb5c6e73e22d7bd
firmo holanda <firmo@viize.com>
2020-03-02 17:00:46 -0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,68 @@
 require 'rails_helper'
 
-class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert false
+RSpec.describe User, type: :model do
+  def setup
+    @user = User.new(name: "example user", email: "example@example.com")
+  end
+
+  it "user should be valid" do
+    assert @user.valid?
+  end
+
+  it "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  it "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  it "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  it "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  it "email validation should accept valid email addresses" do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-er@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  it "email validation should reject invalid email addresses" do
+    invalid_addresses = %w[user@example,com USER_at_foo.com user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..net
+                           foo@bar..bar..baz.com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+
+  it "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  it "email addresses should be saved as lowercase" do
+    address_with_uppercase = "Ex@Example.COM"
+    @user.email = address_with_uppercase
+    @user.save
+    assert_equal address_with_uppercase.downcase, @user.reload.email
+  end
+
+  it "order should be most recent first" do
+    assert_equal users(:most_recent), User.first
   end
 end
