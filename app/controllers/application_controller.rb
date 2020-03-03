2825d40d5550dcd4839a65f9d1323a1d88e09039
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-03 17:37:30 -0500

    Fix the errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,29 +7,26 @@ class ApplicationController < ActionController::Base
 
   # Confirms a logged in user.
   def logged_in_user
-    unless logged_in?
+    return if logged_in?
       store_location
       flash[:danger] = 'please log in.'
       redirect_to login_url
-    end
   end
 
   # Confirms a current user as the host of the target event or invite.
   def host_user
     @event = current_user.events_as_host.find_by(id: params[:id])
     @event ||= current_user.events_as_host.find_by(id: params[:event_id])
-    if @event.nil?
+    return unless @event.nil?
       flash[:danger] = 'access denied: you are not the host.'
       redirect_to root_url
-    end
   end
 
   # Confirms a current user as a guest of the target invite.
   def guest_user
-    unless current_user.id == params[:user_id].to_i
+    return if current_user.id == params[:user_id].to_i
       flash[:danger] = 'access denied: you are not the correct user.'
       redirect_to root_url
-    end
   end
 
   # Confirms a current user as a host or guest of the target invite.
